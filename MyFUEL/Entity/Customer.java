ac7f92095774d06621193956ab204238638f92b5
G7 <meitali2007@gmail.com>
2015-12-17 11:09:45 +0200

    hfhf

--- MyFUEL/Entity/Customer.java
+++ MyFUEL/Entity/Customer.java
@@ -5,29 +5,27 @@ public class Customer {
 	protected CustomerType customerType;
 	protected String CustomerID;
 
-	/**
-	 * 
-	 * @param customerType
-	 */
+
 	/*public Customer(CustomerType type) {
 		//throw new UnsupportedOperationException();
 		this.customerType = type;
 	}*/
 
 	public void getCustomerType() {
-		throw new UnsupportedOperationException();
+		System.out.println("");
+	//	throw new UnsupportedOperationException();
 	}
 
 	public void setCustomerType() {
-		throw new UnsupportedOperationException();
+	//	throw new UnsupportedOperationException();
 	}
 
 	public void getCustomerID() {
-		throw new UnsupportedOperationException();
+	//	throw new UnsupportedOperationException();
 	}
 
 	public void setCustomerID() {
-		throw new UnsupportedOperationException();
+	//	throw new UnsupportedOperationException();
 	}
 
 }
