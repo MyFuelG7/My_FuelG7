6d0f1069c50581cd8514fbcbf663daf56729e309
hofit93 <hofit93@gmail.com>
2015-12-21 21:20:49 +0200

    main server updated

--- MyFUEL/Server/Server.java
+++ MyFUEL/Server/Server.java
@@ -2,12 +2,12 @@ package Server;
 //g7
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import ServerController.DBController;
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
-
-
 import commen.Com;
 
 public class Server extends AbstractServer {
@@ -17,7 +17,7 @@ public class Server extends AbstractServer {
 	 * The default port to listen on.
 	 */
 	// final public static int DEFAULT_PORT = 5555;
-	private Connection conn;
+	private static Connection conn;
 	private DBController controller;
 
 	// Constructors ****************************************************
@@ -92,6 +92,73 @@ public class Server extends AbstractServer {
 		System.out.println("Server => clientException");
 		exception.printStackTrace();
 	}
+	  /**
+	   * This method is responsible for the creation of 
+	   * the server instance (there is no UI in this phase).
+	   *
+	   * @param args[0] The port number to listen on.  Defaults to 5555 
+	   *          if no argument is entered.
+	   */
+	  public static void main(String[] args) 
+	  {
+		  int port = 0; //Port to listen on
+		  String user = "root";
+		  String password = "Braude";
+		  
+
+		  try
+		  {
+			  user = args[0];			//user
+			  password = args[1];		//password
+			  System.out.println("Credentials are: " + args[0] + " " + args[1]);
+		  }
+		  
+		  catch(ArrayIndexOutOfBoundsException e)
+		  {
+			  user = "root";			//user
+			  password = "Braude";		//password
+		  }
+		    
+		  try 
+		  {
+			  Class.forName("com.mysql.jdbc.Driver").newInstance();
+		  } catch (Exception ex) {/* handle the error*/}
+	    
+		  try 
+		  {
+			  conn = DriverManager.getConnection("jdbc:mysql://localhost/test", user, password);
+			  System.out.println("SQL connection succeed");
+		  } catch (SQLException ex) 
+		  {/* handle any errors*/
+			  System.out.println("SQLException: " + ex.getMessage());
+			  System.out.println("SQLState: " + ex.getSQLState());
+			  System.out.println("VendorError: " + ex.getErrorCode());
+		  }
+		  catch (ArrayIndexOutOfBoundsException ex){
+			  System.out.println("Command line parametres: [listening port] [DB user] [DB password]");
+			  System.exit(1);
+		  }
+
+		  try
+		  {
+			  port = Integer.parseInt(args[0]); //Get port from command line
+		  }
+		  catch(Throwable t)
+		  {
+			  port = 3306; //Set port to 3306
+		  }
+		
+		  Server sv = new Server(port);
+	    
+		  try 
+		  {
+			  sv.listen(); //Start listening for connections
+		  } 
+		  catch (Exception ex) 
+		  {
+			  System.out.println("ERROR - Could not listen for clients!");
+		  }
+	  }
 
 }
 
